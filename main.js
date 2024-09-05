bcb2a87fac75090150ae7616a740fe8559e1af19
Bohdan Khmil <khmil.work@gmail.com>
2024-09-05 08:00:22 +0300

    hw-1 is reworked

--- main.js
+++ main.js
@@ -28,6 +28,8 @@ const Separators = {
 // ліміт для циклів
 const LIMIT = 5;
 
+const commonDirsPaths = [];
+const commonFilesPaths = [];
 
 // основна ідея в тому щоб динамічно формувати назви,
 // бо в теорії можна було бб завести масиви і зробити через Promise.allSettled
@@ -49,28 +51,42 @@ const main = async () => {
             // ну тут все те саме що й вище було
             const pathToCommonFolder = path.join(pathToBaseFolder, FoldersNames.commonFolderName + (i + 1));
 
-            console.log('\n' + Separators.folder);
-            console.log('Common folder #' + (i + 1) + ':');
-
             await fs.mkdir(pathToCommonFolder);
-
-            console.log('Path => ' + pathToCommonFolder);
-            // створюю об'єкт типу Stats в якого вже беру необхідні методи для перевірки
-            console.log('isDirectory => ' + (await fs.stat(pathToCommonFolder)).isDirectory());
-            console.log('isFile => ' + (await fs.stat(pathToCommonFolder)).isFile());
 
             for (let j = 0; j < LIMIT; ++j) {
                 // і тут те саме, тільки пишу вже файл
                 const pathToCommonFile = path.join(pathToCommonFolder, FilesNames.commonFileName + (j + 1) + Exts.txt);
 
-                console.log(Separators.file);
-                console.log('\tCommon file #' + (j + 1) + ' from folder #' + (i + 1) + ':');
-
                 await fs.writeFile(pathToCommonFile, i.toString());
 
-                console.log('\tPath => ' + pathToCommonFile);
-                console.log('\tisDirectory => ' + (await fs.stat(pathToCommonFile)).isDirectory());
-                console.log('\tisFile => ' + (await fs.stat(pathToCommonFile)).isFile());
+                commonFilesPaths.push(pathToCommonFile);
+            }
+
+            commonDirsPaths.push(pathToCommonFolder);
+        }
+
+        const elements = await fs.readdir(pathToBaseFolder);
+
+        for (let i = 0; i < elements.length; ++i) {
+            console.log('\n' + Separators.folder);
+
+            const dirCounter = elements[i].split('folder')[1];
+            console.log('Common folder #' + dirCounter + ':');
+
+            console.log('Path => ' + commonDirsPaths[i]);
+            // створюю об'єкт типу Stats в якого вже беру необхідні методи для перевірки
+            console.log('isDirectory => ' + (await fs.stat(commonDirsPaths[i])).isDirectory());
+            console.log('isFile => ' + (await fs.stat(commonDirsPaths[i])).isFile());
+
+            const items = await fs.readdir(commonDirsPaths[i]);
+
+            for (let j = 0; j < items.length; ++j) {
+                console.log(Separators.file);
+                console.log('\tCommon file #' + (items[j].split('file')[1][0]) + ' from folder #' + dirCounter + ':');
+
+                console.log('\tPath => ' + commonFilesPaths[j]);
+                console.log('\tisDirectory => ' + (await fs.stat(commonFilesPaths[j])).isDirectory());
+                console.log('\tisFile => ' + (await fs.stat(commonFilesPaths[j])).isFile());
                 console.log(Separators.file);
             }
 
@@ -78,6 +94,7 @@ const main = async () => {
         }
     } catch (e) {
         console.error('1000-7');
+        console.log(e);
     }
 }
 
